8a7f45de792d2f3aff433d916dce4db6c0f1935e
Darsh Jogi <darshjogi001@gmail.com>
2024-08-22 16:12:35 +0530

    images are changed now

--- src/current-weather.jsx
+++ src/current-weather.jsx
@@ -49,16 +49,16 @@ function CurrentWeather({ Current_weather_data_obj, location_deatils }) {
     ]
 
     let Current_weather_icons = [
-        "/assets/Weather-icons/temperature.png",
-        "/assets/Weather-icons/Precipitation.png",
-        "/assets/Weather-icons/Rain.png",
-        "/assets/Weather-icons/Humidity.png",
-        "/assets/Weather-icons/Cloud_cover.png",
-        "/assets/Weather-icons/Snow-fall.png",
-        "/assets/Weather-icons/Surface_pressure.png",
-        "/assets/Weather-icons/Sealevel-pressure.png",
-        "/assets/Weather-icons/Wind_speed.png",
-        "/assets/Weather-icons/Wind_direction.png",
+        "src/assets/Weather-icons/temperature.png",
+        "src/assets/Weather-icons/Precipitation.png",
+        "src/assets/Weather-icons/Rain.png",
+        "src/assets/Weather-icons/Humidity.png",
+        "src/assets/Weather-icons/Cloud_cover.png",
+        "src/assets/Weather-icons/Snow-fall.png",
+        "src/assets/Weather-icons/Surface_pressure.png",
+        "src/assets/Weather-icons/Sealevel-pressure.png",
+        "src/assets/Weather-icons/Wind_speed.png",
+        "src/assets/Weather-icons/Wind_direction.png",
     ]
 
     let Current_weather_items_elements = []
